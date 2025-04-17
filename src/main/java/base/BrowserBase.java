fffa1fd1d6a2970d1569cab00226aad354b6f9ac
Dulaxx <tharidukad@gmail.com>
2025-04-17 10:15:45 +0200

    update browser

--- src/main/java/base/BrowserBase.java
+++ src/main/java/base/BrowserBase.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BrowserBase {
     protected WebDriver driver;
@@ -15,8 +17,15 @@ public class BrowserBase {
     public WebDriver initializeDriver(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        Map<String, Object> prefs = new HashMap<>();
+        Map<String, Object> contentSettings = new HashMap<>();
+        contentSettings.put("popups", 2); // 2 blocks pop-ups
+        prefs.put("profile", new HashMap<String, Object>() {{
+            put("managed_default_content_settings", contentSettings);
+        }});
+        options.setExperimentalOption("prefs", prefs);
+
         options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking"));
-        caps.setCapability(ChromeOptions.CAPABILITY, options);
 
         this.driver = new ChromeDriver(options);
         this.driver.manage().window().maximize();
